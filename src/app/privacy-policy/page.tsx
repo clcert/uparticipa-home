070d45aa407b0ef181e0f15540b86764a61dd642
Camilo Gómez <cagomeznu@gmail.com>
2025-09-11 13:00:20 -0300

    Include Privacy Policy

---
+++ src/app/privacy-policy/page.tsx
@@ -0,0 +1,72 @@
+export default function PrivacyPolicy() {
+    return (
+        <section className="section">
+            <div className="container content">
+                <h1 id="pol-tica-de-privacidad" className="title is-2 has-text-weight-bold">POLÍTICA DE PRIVACIDAD</h1>
+                <p>Este documento describe las prácticas de privacidad usadas en la plataforma UParticipa (“el sitio”), y aplican única y exclusivamente a la información recolectada en el sitio.</p>
+                <p>Esta política le informa a usted acerca de:</p>
+                <ul className="list-disc ml-5">
+                    <li>Qué información personal identificable es recolectada a través del sitio, cómo es utilizada, y con quién es compartida.</li>
+                    <li>Qué opciones tiene usted respecto del uso de su información.</li>
+                    <li>Cómo puede usted corregir inexactitudes en la información recolectada.</li>
+                    <li>Qué procedimientos de seguridad son empleados para proteger su información de su mala utilización.</li>
+                </ul>
+                <hr />
+                <h2 id="recolecci-n-uso-y-compartici-n-de-informaci-n" className="title is-4 mt-5">Recolección, uso y compartición de información</h2>
+                <p>La Universidad de Chile (&quot;Nosotros&quot;) es la única propietaria de la información recopilada en este sitio. Solo recolectamos la información enviada por la Junta Electoral Local o autoridad universitaria relevante, y la Vicerrectoría de Tecnologías de Información (VTI) de la Universidad de Chile. No entregamos, vendemos, alquilamos ni regalamos la información recolectada a ninguna persona u organización, excepto en los casos que la ley chilena nos obligue a divulgar el total o parte de dicha información a las autoridades, en cuyo caso lo informaremos de inmediato a menos que la ley aplicable nos exija guardar secreto.</p>
+                <p>Si Ud. nos contacta directamente por alguno de los mecanismos disponibles para ello (correo, teléfono, etc.) usaremos su información para responderle en relación con el motivo por el cual nos contactó. No compartiremos su información con terceros ajenos a nuestra organización, excepto cuando sea necesario para cumplir con su solicitud.</p>
+                <h3 id="informaci-n-recolectada" className="subtitle is-5 mt-4">Información recolectada</h3>
+                <p>La información que recolectamos actualmente sobre Usted al navegar en esta página web, en nuestras operaciones web del día a día, es la siguiente:</p>
+                <ul className="list-disc ml-5">
+                    <li>Su dirección IP.</li>
+                    <li>Su User Agent (información del navegador que usted usa, tal como su nombre, versión y sistema operativo).</li>
+                    <li>Páginas visitadas en nuestro sitio web, incluyendo fecha de visita.</li>
+                </ul>
+                <p><span className="has-text-weight-semibold">Se utilizan para:</span> Realizar estudios de uso sobre las distintas páginas que componen el sitio web, tales como determinación de país del visitante y flujo de visita de páginas web.</p>
+                <p>En los casos en que usted sea miembro de la comunidad de la Universidad de Chile y participe como votante en una votación hospedada en nuestro sistema, manejamos la siguiente información:</p>
+                <p><strong>Información entregada por la Junta Electoral Local o autoridad universitaria competente correspondiente a cada votante:</strong></p>
+                <ul className="list-disc ml-5">
+                    <li>Nombre completo y Rol Único Nacional (RUN) o Número de Pasaporte.<br /><em>Se utiliza para: Identificar a cada persona en el padrón electoral entregado.</em></li>
+                    <li>Correo electrónico asociado a cada votante.<br /><em>Se utiliza para: Contactar a un votante para resolver algún problema o duda que surja durante la elección.</em></li>
+                    <li>Ponderador del votante (opcional): número que representa el peso del voto emitido por el votante, solamente utilizado en caso de elecciones que asignan distintas ponderaciones a distintos miembros del claustro elector.<br /><em>Se utiliza para: Calcular correctamente los votos totales emitidos a cada alternativa de votación.</em></li>
+                </ul>
+                <p><strong>Información entregada por la Vicerrectoría de Tecnologías de la Información (VTI) de la Universidad de Chile:</strong></p>
+                <ul className="list-disc ml-5">
+                    <li>Nombre de Usuario de Cuenta UChile.<br /><em>Se utiliza para: Validar si las personas que inician sesión con su cuenta UChile (pasaporte) pueden votar en la elección o no.</em></li>
+                </ul>
+                <p>En los casos en que usted participe como Custodio de Clave en una votación hospedada en nuestro sistema, manejamos la siguiente información:</p>
+                <ul className="list-disc ml-5">
+                    <li>Nombre de Usuario y Enlace Único de acceso exclusivos para el sistema<br /><em>Se utilizan para: Validar su acceso como Custodio de Clave.</em></li>
+                    <li>Correo electrónico<br /><em>Se utiliza para: Contactar al Custodio de Clave para enviar las instrucciones respectivas, y para resolver algún problema o duda que surja durante la elección.</em></li>
+                </ul>
+                <hr />
+                <h2 id="informaci-n-publicada" className="title is-4 mt-5">Información Publicada</h2>
+                <p>Las elecciones hospedadas por nuestro sistema publican los votos cifrados de cada votante y sus ponderaciones, los cuales pueden ser vistos por cualquier otra persona con capacidad de voto en la misma. Los datos que se publican son:</p>
+                <ul className="list-disc ml-5">
+                    <li>Ponderador de los votantes (anonimizados)</li>
+                    <li>Votos cifrados emitidos, en caso de haber votado al menos una vez.</li>
+                </ul>
+                <p>Estos datos son publicados para permitir a los y las participantes de la votación validar que su voto cifrado fue recibido correctamente y que el resultado de la elección sea consistente con los votos emitidos (usando un mecanismo criptográfico de validación indirecta).</p>
+                <hr />
+                <h2 id="su-acceso-y-control-sobre-la-informaci-n" className="title is-4 mt-5">Su acceso y control sobre la información</h2>
+                <p>Si bien las instancias de contacto son limitadas exclusivamente a casos en los que se presente un problema en una votación habilitada para usted, puede optar por no recibir ningún contacto de nosotros en cualquier momento.</p>
+                <p>Enviando un correo electrónico a <a href="mailto:participa@uchile.cl">participa@uchile.cl</a> usted puede:</p>
+                <ul className="list-disc ml-5">
+                    <li>Consultar qué información tenemos sobre Usted, si la hay.</li>
+                    <li>Cambiar o corregir información acerca de Usted. Si esto altera algún proceso de votación vigente, deberá ser aprobado por la Junta Electoral Local o autoridad universitaria respectiva.</li>
+                    <li>Solicitar que borremos información acerca de Usted. Si esto altera algún proceso de votación vigente, deberá ser aprobado por la Junta Electoral Local o autoridad universitaria respectiva.</li>
+                    <li>Expresar cualquier preocupación que Usted tenga respecto del uso que hacemos de su información.</li>
+                    <li>Solicitar el cese de contacto con Usted.</li>
+                </ul>
+                <hr />
+                <h2 id="seguridad" className="title is-4 mt-5">Seguridad</h2>
+                <p>En UParticipa la seguridad es un tema serio, y por ello tomamos precauciones para proteger su información. Cuando Usted envía información confidencial a través del sitio web, su información está protegida tanto en línea (usando TLS) como fuera de línea.</p>
+                <p>En los casos en que recopilamos datos personales, estos son mantenidos solamente durante el tiempo que se requieren para la preparación y ejecución de la votación. Posterior a su cierre, y en un plazo no mayor a 10 días hábiles, los datos del padrón serán eliminados de nuestros sistemas y de sus respaldos. Los votos cifrados emitidos se mantendrán por un periodo de 12 meses posterior al cierre de la votación, a fin de permitir la verificación criptográfica del correcto cálculo de los resultados de la votación correspondiente.</p>
+                <p>Además de proteger su información transmitida en línea, esta también es protegida fuera de línea en nuestros servidores. Solo los miembros del equipo de UParticipa que requieran realizar alguna tarea específica pueden acceder a información personal identificable. Los computadores y servidores en los cuales almacenamos tal información se encuentran en un ambiente seguro y controlado, con acceso exclusivo a este equipo.</p>
+                <p>Si Usted cree que no estamos cumpliendo con esta política de privacidad, puede comunicarse con nosotros inmediatamente por correo electrónico a <a href="mailto:participa@uchile.cl">participa@uchile.cl</a> para hacernos llegar su denuncia y le contactaremos para aclarar la situación.</p>
+                <hr />
+                <p><em>Revisada por:</em><br />Alejandro Hevia, julio 2022<br />Camilo Gómez, enero 2025</p>
+            </div>
+        </section>
+    );
+}
